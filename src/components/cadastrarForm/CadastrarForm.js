1a2e3979b977c46e227abd90c813d899116a2cce
CunhaRoberto <rcunha@live.com>
2024-07-08 02:11:22 -0300

    .

--- src/components/cadastrarForm/CadastrarForm.js
+++ src/components/cadastrarForm/CadastrarForm.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Select from '../layout/form/Select';
+
 import SubmitButton from '../layout/form/SubmitButton';
 
 
 const CadastrarViagemForm = ({btnText}) => {
     return (
         <form>
-            {/* <Select name='origem_id' text='Selecione a origem'/> */}
+            <p>Select....</p>
             <SubmitButton text={btnText}/>
                        
         </form>
