2e214f441dcdc3a91eeca2caa46e3dcbb5b18217
Roberto Cunha <rcunha@live.com>
2024-08-29 16:57:54 -0300

    ajuste tamanho do card

--- src/components/pages/EmpresasCard.js
+++ src/components/pages/EmpresasCard.js
@@ -3,20 +3,20 @@ import styles from './EmpresasCard.module.css'; // Verifique se o caminho está 
 import LinkButton from '../layout/LinkButton';
 
 const EmpresasCard = ({ id, name, startDate, finishDate }) => {
-    
     return (
         <div className={styles.empresa_card}>
             <h4>{name}</h4>
-            {/* <p><span>Partida:</span> {startDate}</p>
-            <p><span>Chegada:</span> {finishDate} (estimada)</p> */}
+            {/* Descomente estas linhas se precisar exibir as datas */}
+            {/* 
+            <p><span>Partida:</span> {startDate}</p>
+            <p><span>Chegada:</span> {finishDate} (estimada)</p>
+            */}
             
             {/* Contêiner para os botões */}
             <div className={styles.button_container}>
-                <LinkButton to='/registrarvisitas' text='Registrar visita' id={id} />     
-                <LinkButton to='/cadastrarequipamentos' text='Cadastrar equipamento' id={id} />
-                
+                <LinkButton to={`/registrarvisitas/${id}`} text='Registrar visita' />
+                <LinkButton to={`/cadastrarequipamentos/${id}`} text='Cadastrar equipamento' />
             </div>
-           
         </div>
     );
 };
