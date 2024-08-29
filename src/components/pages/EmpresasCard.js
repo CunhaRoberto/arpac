371dd8298e22d2f17865714037f6bcdcaeddfec2
CunhaRoberto <rcunha@live.com>
2024-08-29 12:02:28 -0300

    .

--- src/components/pages/EmpresasCard.js
+++ src/components/pages/EmpresasCard.js
@@ -7,8 +7,11 @@ const EmpresasCard = ({ id, name, startDate, finishDate }) => {
     return (
         <div className={styles.empresa_card}>
             <h4>{name}</h4>
-            {/* <p><span>Partida:</span> {startDate}</p>
-            <p><span>Chegada:</span> {finishDate} (estimada)</p> */}
+            <p><span>Data da última visita:</span>24/08/2024</p>
+            <p><span>Previsão da próxima visita:</span> 30/12/2024</p>
+
+            {/* <p><span>Data da última visita:</span> {startDate}</p>
+            <p><span>Previsão da próxima visita:</span> {finishDate} (estimada)</p> */}
             
             {/* Contêiner para os botões */}
             <div className={styles.button_container}>
