e8b48c1d971e2be755d9162dd4020cd8dbbba599
CunhaRoberto <rcunha@live.com>
2024-09-03 20:11:41 -0300

    add modal para redirecionar para cadastro de equipamento

---
+++ src/components/layout/modal/ModalEquipamento.js
@@ -0,0 +1,19 @@
+const ModalEq = ({ onClose }) => {
+    return (
+        <div style={{
+            position: 'fixed', top: '50%', left: '50%',
+            transform: 'translate(-50%, -50%)', backgroundColor: '#fff',
+            padding: '2rem', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)', zIndex: 1000
+        }}>
+            <h2>Nenhum equipamento encontrado</h2>
+            <p>Não há registros de equipamentos. Deseja cadastrar um novo equipamento?</p>
+            <button onClick={onClose}>Fechar</button>
+            <button onClick={() => {
+                onClose();
+                // Lógica para redirecionar ao cadastro de equipamento
+            }}>Cadastrar Equipamento</button>
+        </div>
+    );
+};
+
+export default ModalEq;
--- src/components/registrarVisitasForm/RegistrarVisitasForm.js
+++ src/components/registrarVisitasForm/RegistrarVisitasForm.js
@@ -2,14 +2,15 @@ import { useEffect, useState } from 'react';
 import Select from '../layout/form/Select';
 import Input from '../layout/form/Input';
 import SubmitButton from '../layout/form/SubmitButton';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
+import Modal from '../layout/modal/Modal';
 
 const RegistrarVisitasForm = ({ handleSubmit, btnText, visitaDto }) => {
     const { id } = useParams();
-    
+    const navigate = useNavigate();
     const [equipamento, setEquipamento] = useState([]);
-    
-    const [visita, setVisita]= useState(() => ({
+    const [showModal, setShowModal] = useState(false);
+    const [visita, setVisita] = useState(() => ({
         ...visitaDto,
         idEmpresa: id 
     }));
@@ -20,8 +21,6 @@ const RegistrarVisitasForm = ({ handleSubmit, btnText, visitaDto }) => {
         { idRevisao: 2, name: 'Revisão 2K' },
         { idRevisao: 3, name: 'Revisão 4K' },
         { idRevisao: 4, name: 'Revisão 8K' }
-        
-        
     ]);
     
     const idEmpresa = id;
@@ -38,6 +37,7 @@ const RegistrarVisitasForm = ({ handleSubmit, btnText, visitaDto }) => {
             .then((data) => {
                 if (data.length === 0) {
                     setEquipamento([{ idEquipamento: '', name: 'Não há registros' }]);
+                    setShowModal(true); // Exibir a modal se não houver registros
                 } else {
                     setEquipamento(data);
                 }
@@ -46,13 +46,17 @@ const RegistrarVisitasForm = ({ handleSubmit, btnText, visitaDto }) => {
         }
     }, [idEmpresa]);
 
+    const handleRedirectToCadastro = () => {
+        setShowModal(false);
+        navigate(`/cadastrarequipamentos/${idEmpresa}`) // Redireciona para a página de cadastro de equipamento
+    };
+
     const submit = (e) => {
         e.preventDefault();
         const newErrors = validate();
         if (Object.keys(newErrors).length > 0) {
             setErrors(newErrors);
         } else {
-            console.log(visita);
             setErrors({});
             handleSubmit(visita);
         }
@@ -60,86 +64,97 @@ const RegistrarVisitasForm = ({ handleSubmit, btnText, visitaDto }) => {
 
     const handleChange = (e) => {
         setVisita({...visita, [e.target.name]: e.target.value});
-    }
+    };
 
     const handleEquipamento = (e) => {
         setVisita({
             ...visita,
             idEquipamento: e.target.value,
-        
         });
-        
-    }
+    };
 
     const handleTipoRevisao = (e) => {
         setVisita({
             ...visita,
             idRevisao: e.target.value                       
         });
-    }
+    };
     
     const validate = () => {
-        console.log(visita)
         const newErrors = {};          
-        if (!visita.dataVisita) newErrors.dataVisita = 'Informe a data da visita.'  
-          
-        if (!visita.horasEquipamento) newErrors.horasEquipamento = 'Informe as horas do equipamento.'   
+        if (!visita.dataVisita) newErrors.dataVisita = 'Informe a data da visita.';  
+        if (!visita.horasEquipamento) newErrors.horasEquipamento = 'Informe as horas do equipamento.';   
         if (!visita.idEquipamento || visita.idEquipamento === '' || visita.idEquipamento === 'Selecione....') {
             newErrors.idEquipamento = 'Selecione um equipamento.';
         } 
-
         if (visita.idEquipamento === 'Não há registros') {
-            newErrors.idEquipamento = 'Cadastre um equipamento para regristrar a visita.';
+            newErrors.idEquipamento = 'Cadastre um equipamento para registrar a visita.';
         }
-
         if (!visita.idRevisao || visita.idRevisao === '' || visita.idRevisao === 'Selecione....') {
             newErrors.idRevisao = 'Selecione o tipo de revisão.';
-        }  
-       
+        }
         return newErrors;
     };
 
+    const handleCloseModal = () => {
+        setShowModal(false); // Fechar a modal e exibir o formulário novamente
+    };
+
     return (
-        <form onSubmit={submit}>
-            {errors.dataVisita && <p style={{ color: 'red', fontSize: '16px', marginBottom: '0.25rem' }}>{errors.dataVisita}</p>}  
-            <Input 
-                type='datetime-local' 
-                text='Data da visita' 
-                name='dataVisita'               
-                value={visita.dataVisita || ''}
-                handleOnChange={handleChange}
-            />
+        <>
+            {showModal && (
+                <Modal
+                    show={true}
+                    onClose={handleCloseModal}
+                    onConfirm={handleRedirectToCadastro}
+                    title={`Não há registros de equipamentos`}
+                    message={` Deseja cadastrar um equipamento agora?`}
+                />
+            )}
 
-            {errors.idEquipamento && <p style={{ color: 'red', fontSize: '16px', marginBottom: '0.25rem' }}>{errors.idEquipamento}</p>}
-            <Select 
-                name='idEquipamento' 
-                text='Selecione o equipamento'
-                options={equipamento} 
-                handleOnChange={handleEquipamento}
-                value={visita.idEquipamento ? visita.idEquipamento : ''}
-            />
+            {!showModal && (
+                <form onSubmit={submit}>
+                    {errors.dataVisita && <p style={{ color: 'red', fontSize: '16px', marginBottom: '0.25rem' }}>{errors.dataVisita}</p>}  
+                    <Input 
+                        type='datetime-local' 
+                        text='Data da visita' 
+                        name='dataVisita'               
+                        value={visita.dataVisita || ''}
+                        handleOnChange={handleChange}
+                    />
 
-            {errors.horasEquipamento && <p style={{ color: 'red', fontSize: '16px', marginBottom: '0.25rem' }}>{errors.horasEquipamento}</p>}
-            <Input 
-                type='number' 
-                text='Quantidade de horas'
-                name='horasEquipamento' 
-                placeholder='Insira a quantidade de horas'
-                value={visita.horasEquipamento || ''}
-                handleOnChange={handleChange}             
-            />
+                    {errors.idEquipamento && <p style={{ color: 'red', fontSize: '16px', marginBottom: '0.25rem' }}>{errors.idEquipamento}</p>}
+                    <Select 
+                        name='idEquipamento' 
+                        text='Selecione o equipamento'
+                        options={equipamento} 
+                        handleOnChange={handleEquipamento}
+                        value={visita.idEquipamento ? visita.idEquipamento : ''}
+                    />
 
-            {errors.idRevisao && <p style={{ color: 'red', fontSize: '16px', marginBottom: '0.25rem' }}>{errors.idRevisao}</p>}
-            <Select 
-                name='idRevisao' 
-                text='Selecione o tipo de revisão'
-                options={tipoRevisao}
-                handleOnChange={handleTipoRevisao}
-                value={visita.idRevisao ? visita.idRevisao : ''}
-            />
+                    {errors.horasEquipamento && <p style={{ color: 'red', fontSize: '16px', marginBottom: '0.25rem' }}>{errors.horasEquipamento}</p>}
+                    <Input 
+                        type='number' 
+                        text='Quantidade de horas'
+                        name='horasEquipamento' 
+                        placeholder='Insira a quantidade de horas'
+                        value={visita.horasEquipamento || ''}
+                        handleOnChange={handleChange}             
+                    />
 
-            <SubmitButton text={btnText}/>
-        </form>
+                    {errors.idRevisao && <p style={{ color: 'red', fontSize: '16px', marginBottom: '0.25rem' }}>{errors.idRevisao}</p>}
+                    <Select 
+                        name='idRevisao' 
+                        text='Selecione o tipo de revisão'
+                        options={tipoRevisao}
+                        handleOnChange={handleTipoRevisao}
+                        value={visita.idRevisao ? visita.idRevisao : ''}
+                    />
+
+                    <SubmitButton text={btnText}/>
+                </form>
+            )}
+        </>
     );
 };
 
